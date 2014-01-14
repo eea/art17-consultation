11e91121b57570f0eb56fa251f97deb9ec9825bd
Alex Morega <alex@grep.ro>
2014-01-14 18:11:51 +0200

    configure logging

--- manage.py
+++ manage.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
+import logging
 from art17.app import create_app, create_manager
 
-
 app = create_app()
-manager = create_manager(app)
+
+
+def main():
+    logging.basicConfig(loglevel=logging.DEBUG)
+    logging.getLogger('alembic').setLevel(logging.INFO)
+    manager = create_manager(app)
+    manager.run()
 
 
 if __name__ == "__main__":
-    manager.run()
+    main()
