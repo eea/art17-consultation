8c22b24179925a315b491d9d627088f7512b045d
Alex Morega <alex@grep.ro>
2014-01-14 16:11:26 +0200

    remove dataset

--- art17/dataset_manager.py
+++ art17/dataset_manager.py
@@ -9,3 +9,14 @@ def ls():
     """ List datasets """
     for dataset in models.Dataset.query:
         print "{d.id}: {d.name}".format(d=dataset)
+
+
+@dataset_manager.command
+def rm(dataset_id):
+    """ Remove a dataset """
+    dataset = models.Dataset.query.get(int(dataset_id))
+    if dataset is None:
+        print "No such dataset"
+        return
+    models.db.session.delete(dataset)
+    models.db.session.commit()
