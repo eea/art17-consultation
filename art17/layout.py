6fd4b757e3f1d4fc615b772d20dbc6aa1c9b1604
Alex Morega <alex@grep.ro>
2014-01-15 16:50:34 +0200

    load layout from zope api

--- art17/layout.py
+++ art17/layout.py
@@ -1,4 +1,5 @@
 import flask
+import requests
 
 layout = flask.Blueprint('layout', __name__)
 
@@ -6,4 +7,17 @@ layout = flask.Blueprint('layout', __name__)
 @layout.record
 def set_up_layout_template(state):
     app = state.app
-    app.jinja_env.globals['layout_template'] = 'layout_default.html'
+    zope_url = app.config.get('LAYOUT_ZOPE_URL')
+
+    if zope_url:
+        app.jinja_env.globals['layout_template'] = 'layout_zope.html'
+        app.before_request(load_zope_template)
+
+    else:
+        app.jinja_env.globals['layout_template'] = 'layout_default.html'
+
+
+def load_zope_template():
+    zope_url = flask.current_app.config['LAYOUT_ZOPE_URL']
+    resp = requests.get(zope_url)
+    flask.g.zope_layout = resp.json()
