2c7e2fddd344d3930208b2454360fb36df3372aa
Alex Eftimie <alex@eftimie.ro>
2014-01-22 11:36:52 +0200

    Move tests outside app module directory.

--- testsuite/conftest.py
+++ testsuite/conftest.py
@@ -1,25 +1,56 @@
-import sys
+from flask.ext.webtest import TestApp
+from pytest import fixture
+from alembic import command, config
 from path import path
-import pytest
 
-sys.path.append(path(__file__).abspath().parent.parent)
+from art17.app import create_app
+from art17.models import db
 
 
-@pytest.fixture
-def app():
-    import flask
-    app = flask.Flask('art17.app')
-    app.config['TESTING'] = True
-    app.config['SECRET_KEY'] = 'foo'
-    from art17.models import db
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
+test_config = {
+    'SERVER_NAME': 'localhost',
+    'TESTING': True,
+    'SECRET_KEY': 'test',
+    'ASSETS_DEBUG': True,
+    'SQLALCHEMY_DATABASE_URI': 'mysql://root@localhost/art17testing',
+    'SQLALCHEMY_MYSQL_URI': 'mysql://root@localhost/',
+}
+
+
+alembic_cfg_path = path(__file__).dirname() / '..' / 'alembic.ini'
+alembic_cfg = config.Config(alembic_cfg_path.abspath())
+
+
+def create_db(url):
+    conn = db.create_engine(url).connect()
+    conn.execute('drop schema if exists art17testing')
+    conn.execute('create schema art17testing')
+    conn.close()
+
+
+def drop_db(url):
+    conn = db.create_engine(url).connect()
+    conn.execute('drop schema art17testing')
+    conn.close()
+
+
+@fixture
+def app(request):
+    app = create_app(test_config)
+    app_context = app.app_context()
+    app_context.push()
+
+    create_db(app.config['SQLALCHEMY_MYSQL_URI'])
+    command.upgrade(alembic_cfg, 'head')
+
+    @request.addfinalizer
+    def fin():
+        drop_db(app.config['SQLALCHEMY_MYSQL_URI'])
+        app_context.pop()
     return app
 
 
-@pytest.fixture
-def summary_app(app):
-    from art17.summary import summary
-    app.register_blueprint(summary)
-    return app
+@fixture
+def client(app):
+    client = TestApp(app, db=db, use_session_scopes=True)
+    return client
