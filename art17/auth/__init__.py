953b3f7b1fbf0b5fccf5fdb8f3dce5c0d107f547
Alex Morega <alex@grep.ro>
2014-01-28 16:42:24 +0200

    move auth providers to separate module

--- art17/auth/__init__.py
+++ art17/auth/__init__.py
@@ -1,17 +1,14 @@
-import logging
 import flask
 from flask.ext.security import Security
 from flask.ext.security import signals as security_signals
 from flask.ext.mail import Message
-import requests
 from art17.auth.security import (
     UserDatastore,
     Art17ConfirmRegisterForm,
     current_user,
 )
 from art17 import models
-
-logger = logging.getLogger(__name__)
+from art17.auth.providers import DebugAuthProvider, ZopeAuthProvider
 
 
 @security_signals.user_confirmed.connect
@@ -72,71 +69,3 @@ def admin_user(user_id):
         return flask.redirect(flask.url_for('.admin_user', user_id=user_id))
 
     return flask.render_template('auth/admin_user.html', user=user)
-
-
-def set_user(user_id):
-    user = models.RegisteredUser.query.get(user_id)
-    if user is None:
-        logger.warn("Autheticated user %r not found in database", user_id)
-    else:
-        if user.is_active():
-            flask.g.user = user
-        else:
-            logger.warn("User %r is marked as inactive", user_id)
-
-
-class DebugAuthProvider(object):
-
-    def init_app(self, app):
-        app.before_request(self.before_request_handler)
-        app.add_url_rule(
-            '/auth_debug',
-            endpoint='auth.debug',
-            methods=['GET', 'POST'],
-            view_func=self.view,
-        )
-        app.context_processor(lambda: {
-            'art17_auth_debug': True,
-        })
-
-    def before_request_handler(self):
-        auth_data = flask.session.get('auth')
-        if auth_data and auth_data.get('user_id'):
-            set_user(user_id=auth_data['user_id'])
-
-    def view(self):
-        auth_debug_allowed = bool(flask.current_app.config.get('AUTH_DEBUG'))
-        if flask.request.method == 'POST':
-            if not auth_debug_allowed:
-                flask.abort(403)
-            user_id = flask.request.form['user_id']
-            if user_id:
-                flask.session['auth'] = {'user_id': user_id}
-            else:
-                flask.session.pop('auth', None)
-            return flask.redirect(flask.url_for('.debug'))
-
-        return flask.render_template('auth_debug.html', **{
-            'user_id': current_user.get_id(),
-            'auth_debug_allowed': auth_debug_allowed,
-        })
-
-
-class ZopeAuthProvider(object):
-
-    def init_app(self, app):
-        self.whoami_url = app.config['AUTH_ZOPE_WHOAMI_URL']
-        app.before_request(self.before_request_handler)
-        app.context_processor(lambda: {
-            'art17_auth_zope': True,
-        })
-
-    def before_request_handler(self):
-        auth_header = flask.request.headers.get('Authorization')
-        resp = requests.get(
-            self.whoami_url,
-            headers={'Authorization': auth_header},
-        )
-        user_id = resp.json()['user_id']
-        if user_id:
-            set_user(user_id=user_id)
