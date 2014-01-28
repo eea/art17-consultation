5795e76bdc2ff288e7748c009db06878714acfdd
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-01-28 11:30:15 +0200

    Added test for species summary view

--- testsuite/test_views.py
+++ testsuite/test_views.py
@@ -2,3 +2,12 @@
 def test_homepage(app, client):
     resp = client.get('/')
     assert resp.status_code == 200
+
+
+def test_species_summary_view(app, client):
+    resp = client.get('/species/summary/',
+                      {'group': 'Mammals', 'period': '1',
+                       'subject': 'Canis lupus', 'region': ''})
+    assert resp.status_code == 200
+    assert resp.content_type == 'text/html'
+    assert 'Canis lupus' in resp.html.find('caption').text
