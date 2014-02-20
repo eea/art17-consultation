0d23f3753e05cd1c3d9b8d69a0f26fa3b1d37c57
Alex Eftimie <alex@eftimie.ro>
2014-02-20 12:56:49 +0200

    fix test factory

--- testsuite/test_views_tables_values.py
+++ testsuite/test_views_tables_values.py
@@ -38,14 +38,17 @@ def setup(app):
     EtcDataHabitattypeAutomaticAssessmentFactory(
         range_surface_area=1283,
         assessment_method='1',
-        habitatcode=1110)
+        habitatcode=1110,
+        region='ALP',
+    )
     HabitattypesManualAssessmentsFactory(
         range_surface_area=1283,
         habitatcode=1110,
         method_range='2XA',
         conclusion_range='FV',
         decision='OK',
-        region='ALP')
+        region='ALP',
+    )
     EtcDicHdHabitat()
     EtcDicMethodFactory()
     db.session.commit()
