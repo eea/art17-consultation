7466b71fd42e7935ec6b3fb43e30a07953df4037
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2014-05-15 15:25:01 +0300

    db: add migration to solve date format inconsistency, ref #19716.

---
+++ alembic/versions/2e83cc0c7306_fix_manual_assessments_date_format.py
@@ -0,0 +1,29 @@
+revision = '2e83cc0c7306'
+down_revision = '169e88c7df10'
+
+from alembic import op
+
+
+SRC_DATE_FORMAT = "%d/%m/%Y %H:%i"
+DST_DATE_FORMAT = "%Y-%m-%d %H:%i"
+
+
+def get_update_query(table_name):
+    return (
+        "UPDATE `{tb_name}` SET last_update = "
+        "DATE_FORMAT(STR_TO_DATE(last_update, '{src_format}'), '{dst_format}')"
+        " WHERE last_update LIKE '%/%';".format(
+            tb_name=table_name,
+            src_format=SRC_DATE_FORMAT,
+            dst_format=DST_DATE_FORMAT,
+        )
+    )
+
+
+def upgrade():
+    op.execute(get_update_query('species_manual_assessment'))
+    op.execute(get_update_query('habitattypes_manual_assessment'))
+
+
+def downgrade():
+    pass
