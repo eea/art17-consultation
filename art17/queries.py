d3d45f14431071885fda87c6a8b9af6ed29a14d7
Alex Eftimie <alex@eftimie.ro>
2015-06-23 11:19:59 +0200

    [factsheet] Fix pressures_threats query - for species.

--- art17/queries.py
+++ art17/queries.py
@@ -1,43 +1,52 @@
 THREATS_QUERY = """
-SELECT A.level2_code                      AS code,
-       C.name_corrected                   AS activity,
-       Round(100 * A.pl2_num / B.pl2_tot) AS pc
-FROM   ((SELECT RS4.level2_code,
-               Count(RS4.pl2_ms) AS pl2_num,
-               1                 AS pl2_set
-        FROM   (SELECT DISTINCT RS2.country AS pl2_ms,
-                                RS2.region,
-                                SUBSTRING_INDEX(pressurecode, '.', 1)
-                                  AS level2_code
-                FROM   ({checklist_table} AS RS3
+
+SELECT    A.level2_code                   AS code,
+          C.name_corrected                AS activity,
+          Round(100*A.pl2_num/B.pl2_tot)  AS pc
+FROM ((SELECT   RS4.level2_code,
+                COUNT(RS4.pl2_ms)   AS pl2_num,
+                1                   AS pl2_set
+       FROM     (SELECT DISTINCT RS2.country AS pl2_ms,
+                        RS2.region,
+                        RS2.speciesname,
+                        SUBSTRING_INDEX(pressurecode, '.', 1) AS level2_code
+                 FROM ({checklist_table} AS RS3
                         INNER JOIN {regions_MS_table} AS RS2
                                 ON (RS3.country = RS2.country)
-                                   AND (RS3.region = RS2.region)
-                                   AND (RS3.{join_column} = RS2.{join_column}))
-                       INNER JOIN data_pressures_threats AS RS1
-                               ON RS3.regionhash = RS1.{regionhash_column}
-                WHERE  (((RS3.{subject_column}) = '{subject}')
-                         AND ((UPPER(presence)) IN
-                           ('1', 'SR TAX', 'LR', 'OP', 'EX'))
-                         AND ((RS1.type_pressure) = '{pressure_type}' )
-                         AND ((RS1.rankingcode) = 'H'))) AS RS4
-        GROUP  BY RS4.level2_code) AS A
-        INNER JOIN (SELECT Count(RS2.region) AS pl2_tot,
-                           1                 AS pl2_set
-                    FROM  {regions_MS_table} AS RS2
-                           INNER JOIN {checklist_table} AS RS3
-                                   ON (RS3.{join_column} = RS2.{join_column})
-                                      AND (RS3.region = RS2.region)
-                                      AND (RS3.country = RS2.country)
-                    WHERE  (RS2.country <> 'GR' )
-                           AND (RS3.{subject_column} = '{subject}')
-                           AND (UPPER(RS3.presence) IN
-                             ('1', 'SR TAX', 'LR', 'OP', 'EX'))) AS B
-                ON A.pl2_set = B.pl2_set)
+                                  AND (RS3.region = RS2.region)
+                                  AND (RS3.{join_column} = RS2.{join_column}))
+                        INNER JOIN data_pressures_threats AS RS1
+                                ON RS3.regionhash = RS1.{regionhash_column}
+                 WHERE (((RS2.country<>'GR') And (RS3.{subject_column}) = '{subject}')
+                      AND (RS2.use_for_statistics = True)
+                      AND (Not (RS1.pressurecode) In ('U','X'))
+                      AND ((RS1.type_pressure)='{pressure_type}')
+                      AND ((RS1.rankingcode)='H')))  AS RS4
+       GROUP BY RS4.level2_code)  AS A
+       INNER JOIN   (SELECT COUNT(T.region) AS pl2_tot,
+                            1               AS pl2_set
+                     FROM   (SELECT DISTINCT RS2.country AS pl2_ms,
+                                    RS2.region,
+                                    RS2.speciesname,
+                                    SUBSTRING_INDEX(pressurecode, '.', 1) AS level2_code
+                             FROM ({checklist_table} AS RS3
+                             INNER JOIN {regions_MS_table} AS RS2
+                                     ON (RS3.country = RS2.country)
+                                        AND (RS3.region = RS2.region)
+                                        AND (RS3.{join_column} = RS2.{join_column}))
+                             INNER JOIN data_pressures_threats AS RS1
+                                     ON RS3.regionhash = RS1.{regionhash_column}
+                             WHERE (((RS2.country<>'GR')
+                                    And (RS3.assessment_speciesname) = '{subject}')
+                                    AND (RS2.use_for_statistics = True)
+                                    AND (Not (RS1.pressurecode) In ('U','X'))
+                                    AND ((RS1.type_pressure)='{pressure_type}')
+                                    AND ((RS1.rankingcode)='H'))) AS T)  AS B
+              ON A.pl2_set = B.pl2_set)
        LEFT JOIN lu_threats AS C
               ON A.level2_code = C.code
-ORDER  BY Round(100 * A.pl2_num / B.pl2_tot) DESC
-LIMIT 10;
+ORDER BY Round(100*A.pl2_num/B.pl2_tot) DESC
+LIMIT 10
 """
 
 COVERAGE_QUERY_HABITAT = """
