497a6c1803818edfd93c173eaba268d3dade474e
Diana Boiangiu <boiangiudiana58@gmail.com>
2018-03-20 14:41:52 +0200

    [refs #92142] Add models to import_greece comand

--- art17/management/import_greece.py
+++ art17/management/import_greece.py
@@ -15,11 +15,14 @@ class ImportGreeceCommand(BaseCreateUserCommand):
     )
 
     def run(self, **kwargs):
+        print("Importing period 2007-2012bis dictionaries...")
         habitatcodes = [
             data.habitatcode for data in
             models.EtcDataHabitattypeAutomaticAssessment.query.all()
             if data.dataset_id == 4
         ]
+
+        print("Importing EtcDicHdHabitat...")
         for habitatcode in habitatcodes:
             hdhabitat = models.EtcDicHdHabitat.query.filter_by(
                 habcode=habitatcode
@@ -39,6 +42,7 @@ class ImportGreeceCommand(BaseCreateUserCommand):
                 models.db.session.add(new_hdhabitat)
                 models.db.session.commit()
 
+        print("Importing EtcDicMethod...")
         etc_dic_methods = models.EtcDicMethod.query.all()
         for method in etc_dic_methods:
             if not models.EtcDicMethod.query.filter_by(
@@ -51,6 +55,101 @@ class ImportGreeceCommand(BaseCreateUserCommand):
                 )
                 models.db.session.add(new_method)
                 models.db.session.commit()
+
+        print("Importing EtcDicGeoregs...")
+        etc_dic_biogeoregs = models.EtcDicBiogeoreg.query.all()
+        for biogeoreg in etc_dic_biogeoregs:
+            if not models.EtcDicBiogeoreg.query.filter_by(
+                    reg_code=biogeoreg.reg_code, dataset_id=4).all():
+                new_biogeoreg = models.EtcDicBiogeoreg(
+                    reg_code=biogeoreg.reg_code,
+                    reg_name=biogeoreg.reg_name,
+                    ordine=biogeoreg.ordine,
+                    order=biogeoreg.order,
+                    dataset_id=4,
+                )
+                models.db.session.add(new_biogeoreg)
+                models.db.session.commit()
+
+        print("Importing EtcDicConclusions...")
+        etc_dic_conclusions = models.EtcDicConclusion.query.all()
+        for conclusion in etc_dic_conclusions:
+            if not models.EtcDicConclusion.query.filter_by(
+                    conclusion=conclusion.conclusion, dataset_id=4).all():
+                new_conclusion = models.EtcDicConclusion(
+                    order=conclusion.order,
+                    conclusion=conclusion.conclusion,
+                    details=conclusion.details,
+                    dataset_id=4
+                )
+                models.db.session.add(new_conclusion)
+                models.db.session.commit()
+
+        print("Importing EtcDicDecision...")
+        etc_dic_decisions = models.EtcDicDecision.query.all()
+        for decision in etc_dic_decisions:
+            if not models.EtcDicDecision.query.filter_by(
+                    decision=decision.decision, dataset_id=4).all():
+                new_decision = models.EtcDicDecision(
+                    order=decision.order,
+                    decision=decision.decision,
+                    details=decision.details,
+                    dataset_id=4,
+                )
+                models.db.session.add(new_decision)
+                models.db.session.commit()
+
+        print("Importing EtcDicPopulationUnit...")
+        etc_dic_population_units = models.EtcDicPopulationUnit.query.all()
+        for unit in etc_dic_population_units:
+            if not models.EtcDicPopulationUnit.query.filter_by(
+                    population_units=unit.population_units,
+                    dataset_id=4).all():
+                new_unit = models.EtcDicPopulationUnit(
+                    order=unit.order,
+                    population_units=unit.population_units,
+                    details=unit.details,
+                    code=unit.code,
+                    dataset_id=4,
+                )
+                models.db.session.add(new_unit)
+                models.db.session.commit()
+
+        print("Importing EtcDicSpeciesType...")
+        etc_dic_species_types = models.EtcDicSpeciesType.query.all()
+        for type in etc_dic_species_types:
+            if not models.EtcDicSpeciesType.query.filter_by(
+                SpeciesTypeID=type.SpeciesTypeID,
+                dataset_id=4
+            ):
+                new_type = models.EtcDicSpeciesType(
+                    SpeciesTypeID=type.SpeciesTypeID,
+                    SpeciesType=type.SpeciesType,
+                    Assessment=type.Assessment,
+                    Note=type.Note,
+                    abbrev=type.abbrev,
+                    dataset_id=4
+                )
+                models.db.session.add(new_type)
+                models.db.session.commit()
+
+        print("Importing EtcDicTrend...")
+        etc_dic_trends = models.EtcDicTrend.query.all()
+        for trend in etc_dic_trends:
+            if not models.EtcDicTrend.query.filter_by(
+                id=trend.id,
+                dataset_id=4
+            ):
+                new_trend = models.EtcDicTrend(
+                    id=trend.id,
+                    trend=trend.trend,
+                    details=trend.details,
+                    dataset_id=4,
+                )
+                models.db.session.add(new_trend)
+                models.db.session.commit()
+
+
 
 import_greece = Manager()
 import_greece.add_command('run', ImportGreeceCommand())
