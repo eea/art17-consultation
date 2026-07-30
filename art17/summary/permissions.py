51f408cc272af9a7be3f140b99980e810416c994
Diana Boiangiu <boiangiudiana58@gmail.com>
2026-07-30 10:57:22 +0300

    Fix show_assessment issue

--- art17/summary/permissions.py
+++ art17/summary/permissions.py
@@ -77,7 +77,7 @@ def can_view_automatic_assessment(dataset):
     if dataset.is_readonly:
         # the reporting is finished for this period and all information can be public at this point
         return True
-    if dataset.latest and current_user.show_assessment == False:
+    if dataset.latest and (not current_user.is_anonymous and current_user.show_assessment == False):
         return False
     if before_consultation():
         # before consultation Stakeholders and public users cannot see the manual assessments
@@ -105,7 +105,7 @@ def can_view_manual_assessment(dataset):
     if dataset.is_readonly:
         # the reporting is finished for this period and all information can be public at this point
         return True
-    if dataset.latest and current_user.show_assessment == False:
+    if dataset.latest and (not current_user.is_anonymous and current_user.show_assessment == False):
         return False
     if before_consultation():
         # before consultation Stakeholders and public users cannot see the manual assessments
